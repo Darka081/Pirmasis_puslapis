695f1711a9c06e11e8a5de84f32b5372dc6322b6
Darka081 <93864614+Darka081@users.noreply.github.com>
2021-12-12 10:12:25 +0200

    Add files via upload

---
+++ js/script.js
@@ -0,0 +1,8 @@
+<script>
+function doSomething() ;
+let elem = document.getElementById("my-div");
+elem.style.border = "1px solid black";
+elem.style.padding = "10px";
+elem.style.margin = "10px";
+doSomething() ;
+</script>
