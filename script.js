ad71b7bfd199a359c90de44cef51cf0be641fcea
darka081 <93864614+Darka081@users.noreply.github.com>
2021-12-12 15:32:23 +0200

    pradzia

--- script.js
+++
@@ -1,8 +0,0 @@
-<script>
-    function doSomething() ;
-    let elem = document.getElementById("my-div");
-    elem.style.border = "1px solid black";
-    elem.style.padding = "10px";
-    elem.style.margin = "10px";
-    doSomething() ;
-</script>
